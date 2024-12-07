de46ace8fa53903d25d2712b11f1b8d59bf633a9
JhonnSoftware <72659574@continental.edu.pe>
2024-12-06 23:42:10 -0500

    Prueba

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -8,7 +8,7 @@
         <meta name="author" content="" />
         <title>@yield('title', 'Dashboard - SB Admin')</title>
         <!-- <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" /> -->
-        <!-- <link rel="stylesheet" href="{{ secure_asset('css/styles.css') }}" /> -->
+       
         <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
