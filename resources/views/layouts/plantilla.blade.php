17849f16a7253ebd4e08a16f358e0da8385db98d
JhonnSoftware <72659574@continental.edu.pe>
2024-12-06 23:06:47 -0500

    Prueba

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -9,7 +9,7 @@
         <title>@yield('title', 'Dashboard - SB Admin')</title>
         <!-- <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" /> -->
         <link rel="stylesheet" href="{{ secure_asset('css/styles.css') }}" /> 
-        <!-- <link rel="stylesheet" href="{{ asset('css/styles.css') }}"> -->
+
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <!-- Bootstrap JS (Bundle incluye Popper.js) -->
